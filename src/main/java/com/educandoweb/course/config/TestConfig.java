8009eb9cb97ba860bea07e7e6d3a5c079ca1c121
Ulisses Batista Santana <santana.gru@gmail.com>
2024-03-04 10:46:54 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +51,17 @@ public class TestConfig implements CommandLineRunner {
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategrories().add(cat2);
+		p2.getCategrories().add(cat1);
+		p2.getCategrories().add(cat3);
+		p3.getCategrories().add(cat3);
+		p4.getCategrories().add(cat3);
+		p5.getCategrories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
