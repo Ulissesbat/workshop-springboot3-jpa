fb58af0eead8b55aaf6df14d3293befae4a2ccbe
Ulisses Batista Santana <santana.gru@gmail.com>
2024-03-05 11:22:18 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -38,6 +38,7 @@ public class OrderItem implements Serializable {
 		id.setProduct(product);
 	}
 	
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.OrderItem;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long > {
 
-	
+
 }
